1e0f14dfdbab7d4ee9ae5fbef45d8e7a18fefd1f
Adz4a <maratovadilzhan4@gmail.com>
2023-05-07 20:39:39 +0600

    event

--- src/main/java/com/program/controller/EventController.java
+++ src/main/java/com/program/controller/EventController.java
@@ -1,7 +1,16 @@
 package com.program.controller;
 
+import com.program.repository.EventRepository;
+import com.program.service.EventService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class EventController {
+
+    @Autowired
+    EventService eventService;
+
+    @Autowired
+    EventRepository eventRepository;
 }
