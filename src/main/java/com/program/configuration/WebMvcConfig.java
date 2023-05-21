b46232df59e675fa1a3277dca47a3e34e1f5b451
miradis <mrametist89@gmail.com>
2023-05-21 23:47:39 +0600

    configuration

--- src/main/java/com/program/configuration/WebMvcConfig.java
+++ src/main/java/com/program/configuration/WebMvcConfig.java
@@ -1,14 +1,16 @@
 package com.program.configuration;
 
 
+import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
+@Configuration
 public class WebMvcConfig implements WebMvcConfigurer {
 
 //    @Value("cors.allowedOrings")
 //    private String allowedOrigins;
-
+    @Override
     public void addCorsMappings(CorsRegistry registry) {
         final long MAX_AGE_SECS = 3600;
 
